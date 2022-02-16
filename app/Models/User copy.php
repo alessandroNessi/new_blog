63af92eeca893993f3c04fff623f1d87bcafa03c
Alessandro Nessi <alessandronessi@MacBook-Pro-di-Alessandro.local>
2022-02-16 19:16:40 +0100

    migration fatte

--- app/Models/User copy.php
+++ app/Models/User copy.php
@@ -19,7 +19,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var string[]
      */
     protected $fillable = [
-        'first_name','last_name','picture','subscription', 'email',
+        'name', 'email',
     ];
 
     /**
